5d0763425f5f989b010c95ddfd12cb8b5eede10b
Sulcan-bit <rgsully@hotmail.com>
2026-04-07 18:56:11 -0600

    Fix: update import path for userProfileValidation

--- app/users/profile/page.tsx
+++ app/users/profile/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { UserProfileFormValues } from "../../../../shared/userProfileValidation";
+import { UserProfileFormValues } from "@/shared/userProfileValidation";
 
 type UserProfile = {
   firstName: string;
